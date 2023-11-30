99370983144b806d0f6cd63549bc4205f092a528
DisuToyin <disujt@gmail.com>
2023-11-30 23:11:01 +0100

    fix

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -2,21 +2,3 @@ import supertest from "supertest";
 import app from "../src/index";
 
 const request = supertest(app);
-
-const testUserData = {
-  email: "johndoe@example.com",
-  password: "Pasword@123",
-};
-
-const testPostData = {
-  id: -1,
-  title: "My Post Title",
-  content: "My post content",
-};
-
-test("Should login user", async () => {
-  return request
-    .post("/login")
-    .send({ email: testUserData.email, password: testUserData.password })
-    .expect(200);
-});
